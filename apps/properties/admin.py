188d22f897dfd38313c5980b727fae65846ce5ce
ViewHolder74 <viewholder74@gmail.com>
2023-05-10 09:12:42 +0200

    Feature: Add CRUD factionality to properties

--- apps/properties/admin.py
+++ apps/properties/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Property, PropertyViews
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ["title", "advert_type", "property_type"]
+    list_filter = ["advert_type", "property_type"]
+
+
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(PropertyViews)
