b77c375ed126c822b255c66dfa4de3e95b89f8bb
ViewHolder74 <viewholder74@gmail.com>
2023-05-10 13:50:01 +0200

    Feature: Docker setup with compose

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("api/v1/properties/", include("apps.properties.urls")),
     path("api/v1/ratings/", include("apps.ratings.urls")),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 admin.site.site_header = "Real Estate Admin"
 admin.site.site_title = "Real Estate Admin Portal"
